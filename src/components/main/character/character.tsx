79dfb1b03a76aab0005818fead6c4650fb752150
nkipi <nilda.kipi@rwth-aachen.de>
2020-11-02 20:57:37 +0100

    Fix styling

--- src/components/main/character/character.tsx
+++ src/components/main/character/character.tsx
@@ -4,23 +4,26 @@ import { Card } from "../../layout/card";
 import { Flex } from "../../layout/flex";
 import styles from "./character.module.scss";
 import zothaPortrait from "../../../images/zotha.jpg";
+import { Helper } from "../../styling/helper";
+import { Property } from "../../styling/property";
+import { Heading } from "../../styling/heading";
 
 export const Character: React.FC = () => {
   const { name, characterClass, level, race, alignment } = zotha.character;
   return (
-      <Card>
+    <Card>
+      <Flex className={styles.flex}>
+        <img src={zothaPortrait} alt={"zotha"} className={styles.portrait} />
+        <Heading>{name}</Heading>
         <Flex>
-          <img src={zothaPortrait} alt={"zotha"} className={styles.portrait} />
-          <h1>{name}</h1>
-          <Flex>
-            <h3>{characterClass}</h3>
-            <h3>{level}</h3>
-          </Flex>
-          <Flex>
-            <p>{race}</p>
-            <p>{alignment}</p>
-          </Flex>
+          <Property>{characterClass}</Property>
+          <Property>{level}</Property>
         </Flex>
-      </Card>
+        <Flex>
+          <Helper>{race}</Helper>
+          <Helper>{alignment}</Helper>
+        </Flex>
+      </Flex>
+    </Card>
   );
 };
